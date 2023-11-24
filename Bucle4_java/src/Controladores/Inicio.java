9a0288a5d66ce5861c7cfedfa4e9f85292395b5b
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2023-11-24 08:33:00 +0100

    no message

---
+++ Bucle4_java/src/Controladores/Inicio.java
@@ -0,0 +1,33 @@
+package Controladores;
+
+import java.util.Scanner;
+
+public class Inicio 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	    Scanner sc = new Scanner(System.in);  
+		int numero1 = 0;
+		int numero2 = 1;
+		int suma = 1;
+		 
+        System.out.println("Escriba un numero para la secuencia: ");
+        
+        int secuencia = sc.nextInt();
+        for (int i = 1; i < secuencia; i++) 
+        {
+             
+            System.out.println(suma);
+             
+           
+            suma = numero1 + numero2;
+            numero1 = numero2;
+            numero2 = suma;
+             
+            
+
+	    }
+   }
+}
